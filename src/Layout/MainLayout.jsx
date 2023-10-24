794b1f207761e4dd0dcc1f736790cced2ad20a8c
frustrated018 <niloyd018@gmail.com>
2023-10-24 21:44:27 +0600

    Added a basic responsive navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
-            <div>this is the main Layout</div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
