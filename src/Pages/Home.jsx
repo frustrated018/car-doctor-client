b478865a7deda6bf18dbc47d0df81497d4e2dca0
frustrated018 <niloyd018@gmail.com>
2023-10-25 09:14:42 +0600

    Added a Footer section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
-
 const Home = () => {
-    return (
-        <div>
-            <div className="text-red-600 text-center text-5xl my-40">This is the home page</div>            
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-red-600 text-center text-5xl my-40">
+        This is the home page
+      </div>
+    </div>
+  );
 };
 
 export default Home;
