d01da3af1df057ac9765939a28b6062bb56dc7a4
frustrated018 <niloyd018@gmail.com>
2023-10-26 23:47:59 +0600

    Made the cards exactly as figma

--- src/Components/ServiceCard/ServiceCard.jsx
+++ src/Components/ServiceCard/ServiceCard.jsx
@@ -1,26 +1,30 @@
 import PropTypes from "prop-types";
-import { AiOutlineArrowRight } from "react-icons/ai";
+import { BsArrowRight } from "react-icons/bs";
 
 const ServiceCard = ({ service }) => {
   const { img, title, price } = service;
 
   return (
     <>
-      <div className=" bg-white rounded-[10px] border border-gray-200 pt-5 pb-8 mx-auto flex flex-col justify-center relative">
+      <div className=" bg-white rounded-[10px] border border-gray-200 py-5 mx-auto flex flex-col justify-center ">
         <figure className=" w-[90%] h-[80%] mx-auto rounded-[10px] flex-1">
           <img src={img} className="rounded-[10px] w-full h-full" />
         </figure>
         {/* text */}
         <div className="ml-5">
-          <h3 className="text-neutral-700 text-[25px] font-bold my-5">{title}</h3>
-          <h4 className="text-left text-orange-600 text-xl font-semibold">
-            Price : ${price}
-          </h4>
+          <h3 className="text-neutral-700 text-[25px] font-bold my-5">
+            {title}
+          </h3>
+          <div className="flex justify-around items-center">
+            <h4 className="text-left text-orange-600 text-xl font-semibold">
+              Price : ${price}
+            </h4>
+            {/* Button */}
+            <button className="w-10 h-6 text-[#FF3811]">
+              <BsArrowRight></BsArrowRight>
+            </button>
+          </div>
         </div>
-        {/* Button */}
-        <button className="absolute w-10 h-10 text-[#FF3811] right-0 bottom-0">
-          <AiOutlineArrowRight></AiOutlineArrowRight>
-        </button>
       </div>
     </>
   );
