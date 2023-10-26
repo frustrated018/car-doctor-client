0313f4a873dcb58fb0c96ee6644672285834d1a0
frustrated018 <niloyd018@gmail.com>
2023-10-27 00:21:07 +0600

    Added a basic login page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,8 @@
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home";
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SignUp";
 
 
 const router = createBrowserRouter([
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/signup',
+        element: <SignUp></SignUp>
       },
     ],
   },
